e0272b1ff5ed6e939ed6f768889b571a72a775e6
zimv <472953586@qq.com>
2019-03-27 17:42:30 +0800

    chore: readme

--- client/pages/index/components/intro/index.js
+++ client/pages/index/components/intro/index.js
@@ -1,7 +1,6 @@
 import React,{ useState } from "react";
 import ProcessSsrStyle from "./../../../../components/ProcessSsrStyle";
 import style from "./index.less";
-//注意非page组件直接继承react component
 function Intro() {
   const [intro, setIntro] = useState("intro component of Index.");
   return (
